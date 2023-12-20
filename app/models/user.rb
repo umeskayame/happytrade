91a6d4b2d9b6e51203a19241f69046e18ddf8787
umeskayame <ume122.0321@gmail.com>
2023-12-20 22:07:52 +0900

    userモデルテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,23 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX , message: " is invalid. Include both letters and numbers"}
+  validates :password, format: { with: VALID_PASSWORD_REGEX , message: "が無効です。 半角英数字混合で入力してください"}
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name, format: {
-    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"is invalid. Input full-width characters"
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"が無効です。全角（漢字・ひらがな・カタカナ）で入力してください"
   },on: :create
   validates :first_name, format: {
-    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"is invalid. Input full-width characters"
+    with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"が無効です。全角（漢字・ひらがな・カタカナ）で入力してください"
   },on: :create
   validates :kana_last_name, presence: true
   validates :kana_first_name, presence: true
   validates :kana_last_name, format: {
-    with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters"
+    with: /\A[ァ-ヶー－]+\z/, message:"が無効です。全角（カタカナ）で入力してください"
   },on: :create
   validates :kana_first_name, format: {
-    with: /\A[ァ-ヶー－]+\z/, message:"is invalid. Input full-width katakana characters"
+    with: /\A[ァ-ヶー－]+\z/, message:"が無効です。全角（カタカナ）で入力してください"
   },on: :create
-  validates :birthday, presence: {message:"can't be blank"}
+  validates :birthday, presence: {message:"を入力してください"}
 
 end
