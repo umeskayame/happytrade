685c032e5c5a27f452515d8e2634d2e3ea88f85b
umeskayame <ume122.0321@gmail.com>
2023-12-29 19:14:25 +0900

    出品ページ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -8,9 +9,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   
   validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
   validates :genre_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :wanted_item, presence: true
 
 end
