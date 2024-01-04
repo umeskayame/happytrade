8ac2509a8581686cdcc6af8dab02487bb48a1e21
umeskayame <ume122.0321@gmail.com>
2024-01-04 20:41:07 +0900

    authenticate_user!メソッド設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
+
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :status_id, :prefecture_id, :shipping_day_id, :wanted_item).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :genre_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
