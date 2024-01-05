7dc47497457e28b6234701a267b6ea5e2f4b37ff
umeskayame <ume122.0321@gmail.com>
2024-01-05 20:31:51 +0900

    itemのFactoryBot作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
