13acc19bdb1190ef4d353170b0b52e91db7c1b86
umeskayame <ume122.0321@gmail.com>
2024-01-18 19:58:01 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :items
+  has_many :comments
 
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
