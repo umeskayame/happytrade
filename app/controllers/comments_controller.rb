3cd0ec2ec87224957c63ec04d351c4137d21aff0
umeskayame <ume122.0321@gmail.com>
2024-02-06 18:16:21 +0900

    コメントの条件分岐(修正1)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
     def create
     @comment = Comment.create(comment_params)
     redirect_to "/items/#{@comment.item.id}"
-  end
-
+    end
+  
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, item_id: params[:item_id])
