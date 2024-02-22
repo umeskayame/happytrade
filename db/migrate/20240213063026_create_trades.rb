47a35703a4c1cc4e915b2af76f57605bf8688020
umeskayame <ume122.0321@gmail.com>
2024-02-22 16:00:48 +0900

    デプロイのため修正

--- db/migrate/20240213063026_create_trades.rb
+++ db/migrate/20240213063026_create_trades.rb
@@ -3,8 +3,8 @@ class CreateTrades < ActiveRecord::Migration[7.0]
     create_table :trades do |t|
       t.references :user,               null: false, foreign_key: true
       t.references :item,               null: false, foreign_key: true
-      t.references :buyer_user,         null: false, foreign_key: { to_table: :user }
-      t.references :buyer_item,         null: false, foreign_key: { to_table: :item }
+      t.references :buyer_user,         null: false, foreign_key: { to_table: :users }
+      t.references :buyer_item,         null: false, foreign_key: { to_table: :items }
       t.timestamps
     end
   end
